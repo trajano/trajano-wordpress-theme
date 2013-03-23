35a9e0a3b8c145a8bedac891b317505e82c4829f
Archimedes Trajano <arch@trajano.net>
2013-03-23 03:16:30 -0400

    Integrated bootstrap files into theme
    Added references to the extension points.
    
    Added JQuery libraries from Google CDN which is needed by Bootstrap.

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
   <link rel="profile" href="http://gmpg.org/xfn/11"/>
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen"/>
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/bootstrap.js"></script>
   <?php if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); ?>
   <?php wp_head(); ?>
 </head>
