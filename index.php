0eb48649264b32338b197f40d54b2d3ae30143e2
Archimedes Trajano <arch@trajano.net>
2013-03-23 04:41:54 -0400

    Added a Navbar with home and search capability

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php get_header(); ?>
+<div id="siteNav" class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+
+      <div class="nav-collapse collapse">
+        <form class="navbar-search pull-right" action="?php bloginfo('url'); ?>">
+          <input type="text" name="s" class="search-query" placeholder="<?php _e('Search'); ?>">
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
 <?php get_footer(); ?>
