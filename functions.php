32870da2935ee93abf527309be22f81f95a23a84
Archimedes Trajano <arch@trajano.net>
2013-03-23 05:29:18 -0400

    Added theme functions
    The functions.php file will only be responsible for importing other PHP
    files.
    
    Each function library is stored in its own folder with the same name.
    
    TODOs are added to the theme-options to indicate what options are going to
    be provided to the theme user.

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Function libraries are imported using this file.  This will only contain "require_once" function calls. Theme
+ * specific functions will be prefixed with "twp_"
+ */
+require_once(dirname(__FILE__) . '/theme-options/theme-options.php');
---
+++ theme-options/theme-options.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Theme options.
+ */
+// TODO switch between bootswatch CSS files
+// TODO choose header icon image
+// TODO choose featured article background image
+// TODO choose background image
+// TODO sidebar left or right
