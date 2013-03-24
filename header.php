16e8b00844abc8c3035407655fe2b584dc93e7b7
Archimedes Trajano <arch@trajano.net>
2013-03-23 23:19:12 -0400

    Relocate site navigation bar to header file
    This will allow index.php to focus more on the main area.

--- header.php
+++ header.php
@@ -10,3 +10,33 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<div id="siteNav" class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="<?php bloginfo('url'); ?>"><?php get_home_icon_image(); bloginfo('name'); ?></a>
+
+      <div class="nav-collapse collapse">
+        <form class="navbar-search pull-right" action="?php bloginfo('url'); ?>">
+          <input type="text" name="s" class="search-query" placeholder="<?php _e('Search'); ?>">
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
+<?php
+function get_home_icon_image()
+{
+  $imageUrl = get_theme_mod("trajano_home_icon_image");
+  if (strlen(get_theme_mod("trajano_home_icon_image")) == 0) {
+    echo "<i class='icon-home icon-white'> </i> ";
+  } else {
+    echo sprintf("<img src='%s' alt='' />", $imageUrl);
+  }
+}
+?>
