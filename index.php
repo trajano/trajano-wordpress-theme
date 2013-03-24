76eb3d27598834df5ab127cb104e796424ad9f85
Archimedes Trajano <arch@trajano.net>
2013-03-23 20:55:14 -0400

    Home icon is now driven by theme customization

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-      <a class="brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+      <a class="brand" href="<?php bloginfo('url'); ?>"><?php get_home_icon_image(); bloginfo('name'); ?></a>
 
       <div class="nav-collapse collapse">
         <form class="navbar-search pull-right" action="?php bloginfo('url'); ?>">
@@ -18,4 +18,22 @@
     </div>
   </div>
 </div>
+<?php
+function get_home_icon_image()
+{
+  $imageUrl = get_theme_mod("trajano_home_icon_image");
+  if (strlen(get_theme_mod("trajano_home_icon_image")) == 0) {
+    echo "<i class='icon-home icon-white'> </i> ";
+  } else {
+    echo sprintf("<img src='%s' alt='' />", $imageUrl);
+  }
+}
+?>
+<?php if (get_theme_mod("trajano_sidebar_location") == "left") {
+  get_sidebar();
+} ?>
+<?php if (get_theme_mod("trajano_sidebar_location") == "right") {
+  get_sidebar();
+}
+?>
 <?php get_footer(); ?>
