b9067cd718b64d370b97a16b17ec9d6dcc304377
Archimedes Trajano <arch@trajano.net>
2013-03-30 20:01:20 -0400

    Apply masonry on content when needed
    Disables masonry on single column mode of bootstreap

--- js/enable-masonry.js
+++ js/enable-masonry.js
@@ -1,18 +1,30 @@
 "use strict";
+var masonrySettings = {
+  itemSelector: 'article',
+  isAnimated: true,
+  columnWidth: function (containerWidth) {
+    return containerWidth / 6;
+  }
+};
+
 jQuery(function ($) {
-  $('#content').masonry({
-    itemSelector: 'article',
-    isAnimated: true,
-    isFitWidth: true,
-    isResizable: true,
-    animationOptions: {
-      duration: 750,
-      easing: 'linear',
-      queue: false
+
+  function applyMasonry() {
+    if ($(document).width() >= 768) {
+      $('#content').masonry(masonrySettings);
+    } else if ($('#content').hasClass("masonry") && $(document).width() < 768) {
+      $('#content').masonry("destroy");
+    }
+  }
+
+  applyMasonry();
+
+  $(window).resize(applyMasonry);
+
+  $(document.body).bind("post-load", function (event) {
+    if ($('#content').hasClass("masonry")) {
+      $('#content').masonry("appended", $('#content').find("article:not(.masonry-brick)"), true);
     }
   });
 
-  $(document.body).bind("post-load", function (event) {
-    $('#content').masonry("appended", $('#content').find("article:not(.masonry-brick)"), true);
-  });
 });
