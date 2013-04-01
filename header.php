a682b80f1b00fee910f49476b0335d0f1085af77
Archimedes Trajano <arch@trajano.net>
2013-04-01 03:37:13 -0400

    added margin to the bottom of the header
    reintroduced div as the bootstrap class navbar-static sets the margin to 0

--- header.php
+++ header.php
@@ -10,29 +10,30 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header class="navbar navbar-inverse navbar-header navbar-static-top">
-  <div class="navbar-inner">
-    <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
-      <a class="brand" href="<?php bloginfo('url'); ?>"><?php twp_home_icon_image(); bloginfo('name'); ?></a>
+<header>
+  <div class="navbar navbar-inverse navbar-header navbar-static-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="<?php bloginfo('url'); ?>"><?php twp_home_icon_image(); bloginfo('name'); ?></a>
 
-      <div class="pull-right">
-        <form class="navbar-search" action="?php bloginfo('url'); ?>">
-          <div class="input-append">
-            <input type="text" name="s" placeholder="<?php _e('Search'); ?>">
-            <span class="btn"><i class="icon-search"> </i></span>
-          </div>
-        </form>
-        <ul class="nav nav-collapse">
-          <li><a href="<?php echo get_bloginfo_rss('rss2_url'); ?>"><i class="icon-rss icon-white"> </i></a></li>
-          </li>
-        </ul>
+        <div class="pull-right">
+          <form class="navbar-search" action="?php bloginfo('url'); ?>">
+            <div class="input-append">
+              <input type="text" name="s" placeholder="<?php _e('Search'); ?>">
+              <span class="btn"><i class="icon-search"> </i></span>
+            </div>
+          </form>
+          <ul class="nav nav-collapse">
+            <li><a href="<?php echo get_bloginfo_rss('rss2_url'); ?>"><i class="icon-rss icon-white"> </i></a></li>
+            </li>
+          </ul>
+        </div>
       </div>
-
     </div>
   </div>
 </header>
