f2b494a7c20d270f2d6241242a1dd63bf7e08379
Archimedes Trajano <arch@trajano.net>
2013-04-01 07:02:16 -0400

    Turn off imagebox on small devices

--- js/enable-imagebox.js
+++ js/enable-imagebox.js
@@ -1,10 +1,37 @@
 "use strict";
 jQuery(function ($) {
-  $('.storycontent a, a.thumbnail').has('img').colorbox({
-    rel: 'gal',
-    maxWidth: "100%",
-    maxHeight: "100%",
-    fixed: true,
-    scrolling: false
-  });
+
+  var images = $('.storycontent a, a.thumbnail').has('img');
+
+  function applyColorbox() {
+    if (images.length == 0) {
+      return;
+    }
+    $.colorbox.resize();
+    var colorboxEnabled = $(images[0]).hasClass("cboxElement");
+    console.warn(colorboxEnabled);
+    if (!colorboxEnabled && $(document).width() >= 768) {
+      images.colorbox({
+        rel: 'gal',
+        maxWidth: "100%",
+        maxHeight: "100%",
+        fixed: true,
+        scrolling: false
+      });
+    } else if (colorboxEnabled && $(document).width() < 768) {
+      if ($.colorbox.element()) {
+        $(document).on("cbox_closed", function () {
+          $.colorbox.remove();
+        });
+        $.colorbox.close();
+      } else {
+        $.colorbox.remove();
+      }
+    }
+  }
+
+  applyColorbox();
+  $(window).resize(applyColorbox);
+
+
 });
