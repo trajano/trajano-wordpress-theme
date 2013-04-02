c9e9af69c73a4250f13bce9647c10012ef674b93
Archimedes Trajano <arch@trajano.net>
2013-04-02 17:34:00 -0400

    Add an error message when there are no posts

--- home.php
+++ home.php
@@ -6,11 +6,18 @@ if (is_paged()) :
   get_header();
   ?>
   <section class="container">
-    <?php
-    the_post();
-    get_template_part('content/hero-unit', get_post_format());
-    get_template_part('content/loop', get_post_format());
-    ?>
+    <?php if (!have_posts()): ?>
+      <div class="alert alert-block alert-error">
+        <h4><i class="icon-minus-sign"> </i><?php _e("No posts found"); ?></h4>
+        <?php _e("This blog has no posts published."); ?>
+      </div>
+    <?php else: ?>
+      <?php
+      the_post();
+      get_template_part('content/hero-unit', get_post_format());
+      get_template_part('content/loop', get_post_format());
+      ?>
+    <?php endif?>
   </section>
   <?php get_footer();
 endif;
