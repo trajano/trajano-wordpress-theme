3b5bbc1f3da2ffee5a25f000ae3199239234f215
Archimedes Trajano <arch@trajano.net>
2013-04-04 21:43:44 -0400

    JSLint fixes

--- js/enable-imagebox.js
+++ js/enable-imagebox.js
@@ -1,30 +1,30 @@
-"use strict";
 jQuery(function ($) {
+    'use strict';
 
-  var images = $('.storycontent a, a.thumbnail').has('img');
+    var images = $('.storycontent a, a.thumbnail').has('img');
 
-  function applyColorbox() {
-    if (images.length === 0) {
-      return;
+    function applyColorBox() {
+        if (images.length === 0) {
+            return;
+        }
+        var colorBoxEnabled = $(images[0]).hasClass("cboxElement");
+        if (!colorBoxEnabled && $(document).width() >= 768) {
+            images.colorbox({
+                rel: 'gal',
+                maxWidth: "100%",
+                maxHeight: "100%",
+                fixed: true,
+                scrolling: false
+            });
+        } else if (colorBoxEnabled && $(document).width() < 768) {
+            $.colorbox.close();
+
+            $('.cboxElement')
+                .removeData("colorbox")
+                .removeClass("cboxElement");
+        }
     }
-    var colorboxEnabled = $(images[0]).hasClass("cboxElement");
-    if (!colorboxEnabled && $(document).width() >= 768) {
-      images.colorbox({
-        rel: 'gal',
-        maxWidth: "100%",
-        maxHeight: "100%",
-        fixed: true,
-        scrolling: false
-      });
-    } else if (colorboxEnabled && $(document).width() < 768) {
-      $.colorbox.close();
 
-      $('.cboxElement')
-        .removeData("colorbox")
-        .removeClass("cboxElement");
-    }
-  }
-
-  applyColorbox();
-  $(window).resize(applyColorbox);
+    applyColorBox();
+    $(window).resize(applyColorBox);
 });
