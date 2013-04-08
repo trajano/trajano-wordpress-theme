ae93fd7f28a471ad9cd2d9993f475e50fab24c25
Archimedes Trajano <arch@trajano.net>
2013-04-08 00:15:20 -0400

    Split layout and style and added CDN option

--- include/theme-options.php
+++ include/theme-options.php
@@ -5,157 +5,175 @@
 class TwpThemeOptions
 {
 
-  /**
-   * Registers the options.
-   */
-  public function registerOptions($wp_customize)
-  {
-    /*
-     * Styles & Layout section
+    /**
+     * Registers the options.
      */
-    $wp_customize->add_section('styles', array (
-      'title' => __('Styles & Layout'),
-      'priority' => 90,
-    ));
+    public function registerOptions($wp_customize)
+    {
+        /*
+         * Styles section
+         */
+        $wp_customize->add_section('styles', array(
+            'title' => __('Styles'),
+            'priority' => 90,
+        ));
 
-    $wp_customize->add_setting("trajano_bootstrap_css", array (
-      "default" => "css/bootstrap.css",
-      "transport" => "postMessage"
-    ));
-    $wp_customize->add_control("bootstrap_css", array (
-      'label' => __('Bootswatch style'),
-      'section' => 'styles',
-      'settings' => "trajano_bootstrap_css",
-      'type' => 'select',
-      'choices' => $this->getBootswatchThemes()
-    ));
+        $wp_customize->add_setting("trajano_bootstrap_css", array(
+            "default" => "css/bootstrap.css",
+            "transport" => "postMessage"
+        ));
+        $wp_customize->add_control("bootstrap_css", array(
+            'label' => __('Bootswatch style'),
+            'section' => 'styles',
+            'settings' => "trajano_bootstrap_css",
+            'type' => 'select',
+            'choices' => $this->getBootswatchThemes()
+        ));
 
-    $wp_customize->add_setting("trajano_colorbox_css", array (
-      "default" => "colorbox/example1/colorbox.css",
-      "transport" => "postMessage"
-    ));
-    $wp_customize->add_control("colorbox_css", array (
-      'label' => __('Colorbox style'),
-      'section' => 'styles',
-      'settings' => "trajano_colorbox_css",
-      'type' => 'select',
-      'choices' => $this->getColorboxThemes()
-    ));
+        $wp_customize->add_setting("trajano_colorbox_css", array(
+            "default" => "colorbox/example1/colorbox.css",
+            "transport" => "postMessage"
+        ));
+        $wp_customize->add_control("colorbox_css", array(
+            'label' => __('Colorbox style'),
+            'section' => 'styles',
+            'settings' => "trajano_colorbox_css",
+            'type' => 'select',
+            'choices' => $this->getColorboxThemes()
+        ));
 
-    $wp_customize->add_setting("trajano_sidebar_location", array (
-      "default" => "right"
-    ));
-    $wp_customize->add_control("sidebar_location", array (
-      'label' => __('Sidebar location'),
-      'section' => 'styles',
-      'settings' => "trajano_sidebar_location",
-      'type' => 'select',
-      'choices' => array (
-        "left" => __("Sidebar on the left"),
-        "right" => __("Sidebar on the right")
-      )
-    ));
+        $wp_customize->add_setting("trajano_use_cdn", array(
+            "default" => false
+        ));
+        $wp_customize->add_control("use_cdn", array(
+            'label' => __('Use CDN resources where possible'),
+            'section' => 'styles',
+            'settings' => "trajano_use_cdn",
+            'type' => 'checkbox'
+        ));
 
-    $wp_customize->add_setting("trajano_magazine_layout_home", array (
-      "default" => true
-    ));
-    $wp_customize->add_control("magazine_layout_home", array (
-      'label' => __('Magazine layout on home page'),
-      'section' => 'styles',
-      'settings' => "trajano_magazine_layout_home",
-      'type' => 'checkbox'
-    ));
+        /*
+         * Layout section
+         */
+        $wp_customize->add_section('layout', array(
+            'title' => __('Layout'),
+            'priority' => 91,
+        ));
 
-    $wp_customize->add_setting("trajano_magazine_layout_search", array (
-      "default" => true
-    ));
-    $wp_customize->add_control("magazine_layout_search", array (
-      'label' => __('Magazine layout on search page'),
-      'section' => 'styles',
-      'settings' => "trajano_magazine_layout_search",
-      'type' => 'checkbox'
-    ));
+        $wp_customize->add_setting("trajano_magazine_layout_home", array(
+            "default" => true
+        ));
+        $wp_customize->add_control("magazine_layout_home", array(
+            'label' => __('Magazine layout on home page'),
+            'section' => 'layout',
+            'settings' => "trajano_magazine_layout_home",
+            'type' => 'checkbox'
+        ));
 
-    $wp_customize->add_setting("trajano_magazine_layout_category", array (
-      "default" => true
-    ));
-    $wp_customize->add_control("magazine_layout_category", array (
-      'label' => __('Magazine layout on category page'),
-      'section' => 'styles',
-      'settings' => "trajano_magazine_layout_category",
-      'type' => 'checkbox'
-    ));
+        $wp_customize->add_setting("trajano_magazine_layout_search", array(
+            "default" => true
+        ));
+        $wp_customize->add_control("magazine_layout_search", array(
+            'label' => __('Magazine layout on search page'),
+            'section' => 'layout',
+            'settings' => "trajano_magazine_layout_search",
+            'type' => 'checkbox'
+        ));
 
-    $wp_customize->add_setting("trajano_magazine_layout_tag", array (
-      "default" => true
-    ));
-    $wp_customize->add_control("magazine_layout_tag", array (
-      'label' => __('Magazine layout on tag page'),
-      'section' => 'styles',
-      'settings' => "trajano_magazine_layout_tag",
-      'type' => 'checkbox'
-    ));
+        $wp_customize->add_setting("trajano_magazine_layout_category", array(
+            "default" => true
+        ));
+        $wp_customize->add_control("magazine_layout_category", array(
+            'label' => __('Magazine layout on category page'),
+            'section' => 'layout',
+            'settings' => "trajano_magazine_layout_category",
+            'type' => 'checkbox'
+        ));
 
-    $wp_customize->add_setting("trajano_magazine_layout_author", array (
-      "default" => true
-    ));
-    $wp_customize->add_control("magazine_layout_author", array (
-      'label' => __('Magazine layout on author page'),
-      'section' => 'styles',
-      'settings' => "trajano_magazine_layout_author",
-      'type' => 'checkbox'
-    ));
+        $wp_customize->add_setting("trajano_magazine_layout_tag", array(
+            "default" => true
+        ));
+        $wp_customize->add_control("magazine_layout_tag", array(
+            'label' => __('Magazine layout on tag page'),
+            'section' => 'layout',
+            'settings' => "trajano_magazine_layout_tag",
+            'type' => 'checkbox'
+        ));
 
-    $wp_customize->add_setting("trajano_magazine_layout_date", array (
-      "default" => true
-    ));
-    $wp_customize->add_control("magazine_layout_date", array (
-      'label' => __('Magazine layout on dated archive page'),
-      'section' => 'styles',
-      'settings' => "trajano_magazine_layout_date",
-      'type' => 'checkbox'
-    ));
-  }
+        $wp_customize->add_setting("trajano_magazine_layout_author", array(
+            "default" => true
+        ));
+        $wp_customize->add_control("magazine_layout_author", array(
+            'label' => __('Magazine layout on author page'),
+            'section' => 'layout',
+            'settings' => "trajano_magazine_layout_author",
+            'type' => 'checkbox'
+        ));
 
-  /**
-   * Bootswatch themes.  This is read from the bootswatch folder and adds the default Bootstrap theme to the list
-   * explicitly as it is not in the bootswatch folder and it should be the first value.
-   * @return array
-   */
-  private function getBootswatchThemes()
-  {
-    $bootswatchThemes = array ();
-    $bootswatchThemes["css/bootstrap.css"] = __("Default Bootstrap");
-    $bootswatchDir = opendir(get_template_directory() . "/bootswatch");
-    while ($path = readdir($bootswatchDir)) {
-      if ($path == "LICENSE" || $path == "." || $path == ".." || $path == "default" || $path == "img") {
-        continue;
-      }
-      $cssFile = "bootswatch/" . $path . "/bootstrap.css";
-      if (!file_exists(get_template_directory() . "/" . $cssFile)) {
-        continue;
-      }
-      $bootswatchThemes[$cssFile] = $path;
+        $wp_customize->add_setting("trajano_magazine_layout_date", array(
+            "default" => true
+        ));
+        $wp_customize->add_control("magazine_layout_date", array(
+            'label' => __('Magazine layout on dated archive page'),
+            'section' => 'layout',
+            'settings' => "trajano_magazine_layout_date",
+            'type' => 'checkbox'
+        ));
+
+        $wp_customize->add_setting("trajano_sidebar_location", array(
+            "default" => "right"
+        ));
+        $wp_customize->add_control("sidebar_location", array(
+            'label' => __('Sidebar location'),
+            'section' => 'layout',
+            'settings' => "trajano_sidebar_location",
+            'type' => 'select',
+            'choices' => array(
+                "left" => __("Sidebar on the left"),
+                "right" => __("Sidebar on the right")
+            )
+        ));
     }
-    return $bootswatchThemes;
-  }
 
-  private function getColorboxThemes()
-  {
-    $themes = array ();
-    $dir = opendir(get_template_directory() . "/colorbox");
-    while ($path = readdir($dir)) {
-      if ($path == "LICENSE" || $path == "." || $path == ".." || $path == "default" || $path == "img") {
-        continue;
-      }
-      $cssFile = "colorbox/" . $path . "/colorbox.css";
-      if (!file_exists(get_template_directory() . "/" . $cssFile)) {
-        continue;
-      }
-      $themes[$cssFile] = $path;
+    /**
+     * Bootswatch themes.  This is read from the bootswatch folder and adds the default Bootstrap theme to the list
+     * explicitly as it is not in the bootswatch folder and it should be the first value.
+     * @return array
+     */
+    private function getBootswatchThemes()
+    {
+        $bootswatchThemes = array();
+        $bootswatchThemes["css/bootstrap.css"] = __("Default Bootstrap");
+        $bootswatchDir = opendir(get_template_directory() . "/bootswatch");
+        while ($path = readdir($bootswatchDir)) {
+            if ($path == "LICENSE" || $path == "." || $path == ".." || $path == "default" || $path == "img") {
+                continue;
+            }
+            $cssFile = "bootswatch/" . $path . "/bootstrap.css";
+            if (!file_exists(get_template_directory() . "/" . $cssFile)) {
+                continue;
+            }
+            $bootswatchThemes[$cssFile] = $path;
+        }
+        return $bootswatchThemes;
     }
-    return $themes;
-  }
+
+    private function getColorboxThemes()
+    {
+        $themes = array();
+        $dir = opendir(get_template_directory() . "/colorbox");
+        while ($path = readdir($dir)) {
+            if ($path == "LICENSE" || $path == "." || $path == ".." || $path == "default" || $path == "img") {
+                continue;
+            }
+            $cssFile = "colorbox/" . $path . "/colorbox.css";
+            if (!file_exists(get_template_directory() . "/" . $cssFile)) {
+                continue;
+            }
+            $themes[$cssFile] = $path;
+        }
+        return $themes;
+    }
 }
 
 /**
@@ -166,9 +184,9 @@ class TwpThemeOptions
  */
 function twp_customize_register($wp_customize)
 {
-  $theme_options = new TwpThemeOptions();
-  $theme_options->registerOptions($wp_customize);
-  add_action("customize_preview_init", "twp_enqueue_theme_customizer_script");
+    $theme_options = new TwpThemeOptions();
+    $theme_options->registerOptions($wp_customize);
+    add_action("customize_preview_init", "twp_enqueue_theme_customizer_script");
 }
 
 /**
@@ -176,7 +194,7 @@ function twp_customize_register($wp_customize)
  */
 function twp_enqueue_theme_customizer_script()
 {
-  wp_enqueue_script("theme-customizer", get_template_directory_uri() . "/js/theme-customizer.js", array ("jquery"));
+    wp_enqueue_script("theme-customizer", get_template_directory_uri() . "/js/theme-customizer.js", array("jquery"));
 }
 
 add_action("customize_register", "twp_customize_register");
