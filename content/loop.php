a2708b589b09c6c0a019e3d0200ecc8be0b9f794
Archimedes Trajano <arch@trajano.net>
2013-04-07 22:52:33 -0400

    Only enable masonry if magazine layout is enabled.

--- content/loop.php
+++ content/loop.php
@@ -1,18 +1,20 @@
 <?php
-wp_enqueue_script("twp-enable-masonry", get_template_directory_uri() . "/js/enable-masonry.js",
-  array ("jquery-masonry"));
+if (twp_is_magazine_layout()) {
+    wp_enqueue_script("twp-enable-masonry", get_template_directory_uri() . "/js/enable-masonry.js",
+        array("jquery-masonry"));
+}
 ?>
 <div class="row">
-  <?php twp_sidebar("left"); ?>
-  <div class="<?php twp_posts_classes() ?>">
-    <div class="row multiple-posts" id="content">
-      <?php
-      while (have_posts()) {
-        the_post();
-        get_template_part('content', get_post_format());
-      }
-      ?>
+    <?php twp_sidebar("left"); ?>
+    <div class="<?php twp_posts_classes() ?>">
+        <div class="row multiple-posts" id="content">
+            <?php
+            while (have_posts()) {
+                the_post();
+                get_template_part('content', get_post_format());
+            }
+            ?>
+        </div>
     </div>
-  </div>
-  <?php twp_sidebar("right"); ?>
+    <?php twp_sidebar("right"); ?>
 </div>
