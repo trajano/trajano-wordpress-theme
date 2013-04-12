9a77561b6875b676f9b31ceb157fbdec58c0d2d1
Archimedes Trajano <arch@trajano.net>
2013-04-12 18:04:39 -0400

    Masonry only on magazine layout and set up postdata
    Fixes #29

--- content/random-loop.php
+++ content/random-loop.php
@@ -1,19 +1,22 @@
 <?php
-wp_enqueue_script("twp-enable-masonry", get_template_directory_uri() . "/js/enable-masonry.js",
-  array ("jquery-masonry"));
+if (twp_is_magazine_layout()) {
+    wp_enqueue_script("twp-enable-masonry", get_template_directory_uri() . "/js/enable-masonry.js",
+        array("jquery-masonry"));
+}
 ?>
 <div class="row">
-  <?php twp_sidebar("left"); ?>
-  <div class="<?php twp_posts_classes() ?>">
-    <div class="row multiple-posts" id="content">
-      <?php
-      $args = array ('numberposts' => 10, 'orderby' => 'rand');
-      $rand_posts = get_posts($args);
-      foreach ($rand_posts as $post) {
-        get_template_part('content');
-      }
-      ?>
+    <?php twp_sidebar("left"); ?>
+    <div class="<?php twp_posts_classes() ?>">
+        <div class="row multiple-posts" id="content">
+            <?php
+            $args = array('numberposts' => 10, 'orderby' => 'rand');
+            $rand_posts = get_posts($args);
+            foreach ($rand_posts as $post) {
+                setup_postdata($post);
+                get_template_part('content', get_post_format($post->ID));
+            }
+            ?>
+        </div>
     </div>
-  </div>
-  <?php twp_sidebar("right"); ?>
+    <?php twp_sidebar("right"); ?>
 </div>
