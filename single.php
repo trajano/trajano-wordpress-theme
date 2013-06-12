daa84d6196fefda469862cb58e91f2ae7d218891
Archimedes Trajano <arch@trajano.net>
2013-06-12 01:23:04 -0400

    Added support for featured-image posts

--- single.php
+++ single.php
@@ -2,19 +2,37 @@
 wp_enqueue_script("twp-enable-imagebox", get_template_directory_uri() . "/js/enable-imagebox.js",
     array("jquery-colorbox"));
 get_header();
-the_post(); ?>
+the_post();
+
+$featured_image_url = "";
+$show_featured_image_horizontal = false;
+$show_featured_image_embedded = false;
+if (has_post_thumbnail()) {
+    $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id(), "full");
+    $featured_image_url = $featured_image[0];
+    if (($featured_image[1] / $featured_image[2]) >= (12 / 4)) {
+        $show_featured_image_horizontal = true;
+    } else {
+        $show_featured_image_embedded = true;
+    }
+}
+?>
 <section id="post-<?php the_ID(); ?>" <?php post_class(array("container", "narrowcolumn")); ?>>
-    <?php if (has_post_thumbnail()):
-        $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id(), "full")?>
+    <?php if ($show_featured_image_horizontal): ?>
         <div class="row">
-            <div class="thumbnail">
-                <a href="<?php echo $featured_image[0] ?>"><?php the_post_thumbnail("large"); ?></a>
+            <div class="thumbnail full-width">
+                <a href="<?php echo $featured_image_url ?>"><?php the_post_thumbnail("large"); ?></a>
             </div>
         </div>
     <?php endif ?>
     <div class="row">
         <?php twp_sidebar("left"); ?>
         <article class="<?php twp_posts_classes() ?>">
+            <?php if ($show_featured_image_embedded): ?>
+                <div class="thumbnail span3 pull-right">
+                    <a href="<?php echo $featured_image_url ?>"><?php the_post_thumbnail("large"); ?></a>
+                </div>
+            <?php endif ?>
             <h1><a href="<?php the_permalink(); ?>"
                    title="<?php echo esc_attr(sprintf(__('Permalink to %s'), the_title_attribute('echo=0'))); ?>"
                    rel="bookmark"><?php the_title(); ?></a></h1>
